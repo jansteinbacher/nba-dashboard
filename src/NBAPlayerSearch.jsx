3e508a12bb0496195296a4d0809c6befabd83b77
jansteinbacher <jan.steinbacher@stud-mail.uni-wuerzburg.de>
2023-11-20 20:03:09 +0100

    design updates

--- src/NBAPlayerSearch.jsx
+++ src/NBAPlayerSearch.jsx
@@ -55,7 +55,7 @@ const NBAPlayerSearch = () => {
   return (
     <div>
       <div className="container mx-auto mt-12 mb-12 lg:flex items-center justify-content">
-        <div className="w-full lg:w-2/3">
+        <div className="w-full lg:w-2/3 mb-8 lg:mb-0 lg:pr-4">
           <h1 className="text-4xl text-white font-semibold mb-4">
             Discover NBA Stats
           </h1>
@@ -64,16 +64,18 @@ const NBAPlayerSearch = () => {
           </h3>
           <Link
             to={`/player/${randomNumber}`}
-            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 mb-12"
+            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 mb-4 lg:mb-0"
           >
             Explore Random Player
           </Link>
         </div>
         <div className="w-full lg:w-1/3">
-          <img src={Hero} alt="heroimage" className="rounded-lg h-96" />
+          <div className="flex items-center justify-center">
+            <img src={Hero} alt="heroimage" className="rounded-lg h-96" />
+          </div>
         </div>
       </div>
-      <div className="container mx-auto p-8 bg-gray-100 rounded-lg shadow-md mt-24 mb-24">
+      <div className="container mx-auto p-8 bg-gray-100 rounded-lg shadow-md mt-8 mb-8">
         <h1 className="text-3xl font-semibold mb-4">NBA Player Search</h1>
         <div className="mb-4">
           <input
@@ -113,7 +115,7 @@ const NBAPlayerSearch = () => {
             {/* Link to navigate to the detailed player stats page */}
             <Link
               to={`/player/${selectedPlayer}`}
-              className="text-blue-500 hover:underline cursor-pointer"
+              className="text-blue-500 hover:underline cursor-pointer block"
             >
               View Detailed Stats
             </Link>
